e73b2ae3bc7a3ceb15b7d65ec53df650b19e5656
Felix Müller <feelx88@googlemail.com>
2012-11-12 22:15:14 +0100

    Changed behaviour to match irrlicht 1.8 behaviour.

--- src/engine/QuaternionConverter.cpp
+++ src/engine/QuaternionConverter.cpp
@@ -22,10 +22,10 @@
 
 btQuaternion QuaternionConverter::bt( const irr::core::quaternion &irr )
 {
-    return btQuaternion( -irr.X, -irr.Y, -irr.Z, irr.W );
+    return btQuaternion( irr.X, irr.Y, irr.Z, irr.W );
 }
 
 irr::core::quaternion QuaternionConverter::irr( const btQuaternion &bt )
 {
-    return irr::core::quaternion( -bt.getX(), -bt.getY(), -bt.getZ(), bt.getW() );
+    return irr::core::quaternion( bt.getX(), bt.getY(), bt.getZ(), bt.getW() );
 }
