ae929667bd1c5eb56de54ec92898da74035ce738
Felix Müller <feelx88@googlemail.com>
2012-12-15 20:59:57 +0100

    Corrected GPL header.

--- src/engine/PythonBinder.cpp
+++ src/engine/PythonBinder.cpp
@@ -1,20 +1,20 @@
 /*
     Copyright 2012 Felix Müller.
 
-    This file is part of %ProjectName%.
+    This file is part of Explore.
 
-    %ProjectName% is free software: you can redistribute it and/or modify
+    Explore is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    %ProjectName% is distributed in the hope that it will be useful,
+    Explore is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with %ProjectName%.  If not, see <http://www.gnu.org/licenses/>.
+    along with Explore.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "PythonBinder.h"
